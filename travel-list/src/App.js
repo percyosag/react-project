0479aa9c6bb399712db099c872bb2dcdbc4e97d1
percyosag <percybuilder@gmail.com>
2024-12-18 19:18:19 -0500

    form element added

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 2, packed: true },
+];
+
 const App = () => {
   return (
     <div className="app">
@@ -20,14 +26,41 @@ const Logo = () => {
 };
 const Form = () => {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+      </select>
+      <input type="text" placeholder="item..." />
+      <button>Add</button>
+    </form>
   );
 };
 const PackingList = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 };
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>✖️</button>
+    </li>
+  );
+};
+
 const Stats = () => {
   return (
     <footer className="stats">
