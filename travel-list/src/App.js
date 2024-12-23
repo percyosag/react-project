fe064db817ed6b70788b7bbc935a7abe2f54b75f
percyosag <percybuilder@gmail.com>
2024-12-23 01:44:38 -0500

    components separated to new files

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import { PackingList } from "./PackingList";
+import { Stats } from "./Stats";
 
 const App = () => {
   const [items, setItems] = useState([]);
@@ -33,139 +37,6 @@ const App = () => {
       />
       <Stats items={items} />
     </div>
-  );
-};
-
-const Logo = () => {
-  return (
-    <div>
-      <h1>🌴 Far Away 💼</h1>
-    </div>
-  );
-};
-const Form = ({ onAddItems }) => {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-  // const [items, setItems] = useState([]);
-  // const handleAddItems = (item) => {
-  //   setItems((items) => [...items, item]);
-  // };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!description) return;
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
-    setDescription("");
-    setQuantity(1);
-    onAddItems(newItem);
-  };
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => {
-          setQuantity(+e.target.value);
-        }}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="item..."
-        value={description}
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
-      />
-      <button>Add</button>
-    </form>
-  );
-};
-const PackingList = ({ items, onDeleteItem, onToggleItem, onClearList }) => {
-  const [sortBy, setSortBy] = useState("input");
-  let sortedItems;
-  if (sortBy === "input") sortedItems = items;
-  if (sortBy === "description")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => a.description.localeCompare(b.description));
-  if (sortBy === "packed")
-    sortedItems = items
-      .slice()
-      .sort((a, b) => Number(a.packed) - Number(b.packed));
-
-  return (
-    <div className="list">
-      <ul>
-        {sortedItems.map((item) => (
-          <Item
-            item={item}
-            key={item.id}
-            onDeleteItem={onDeleteItem}
-            onToggleItem={onToggleItem}
-          />
-        ))}
-      </ul>
-      <div className="actions">
-        <select
-          value={sortBy}
-          onChange={(e) => {
-            setSortBy(e.target.value);
-          }}
-        >
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
-        </select>
-        <button onClick={onClearList}>Clear-List</button>
-      </div>
-    </div>
-  );
-};
-
-const Item = ({ item, onDeleteItem, onToggleItem }) => {
-  return (
-    <li>
-      <input
-        type="checkbox"
-        value={item.packed}
-        onChange={() => onToggleItem(item.id)}
-      />
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
-      <button onClick={() => onDeleteItem(item.id)}>✖️</button>
-    </li>
-  );
-};
-//Stats Component
-const Stats = ({ items }) => {
-  if (!items.length) {
-    return (
-      <p className="stats">
-        <em>Start adding items to your parking list 🚀</em>
-      </p>
-    );
-  }
-  const numItems = items.length;
-  const numPacked = items.filter((item) => item.packed).length;
-  const percentage = Math.round((numPacked / numItems) * 100);
-
-  return (
-    <footer className="stats">
-      <em>
-        {percentage === 100
-          ? "You,ve got everything! Ready to go ✈️"
-          : `👜 You have ${numItems} items on your list, and you already packed
-        ${numPacked} (${percentage}%)`}
-      </em>
-    </footer>
   );
 };
 
---
+++ travel-list/src/Logo.js
@@ -0,0 +1,9 @@
+const Logo = () => {
+  return (
+    <div>
+      <h1>🌴 Far Away 💼</h1>
+    </div>
+  );
+};
+
+export default Logo;
