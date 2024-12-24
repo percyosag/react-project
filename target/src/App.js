e487a91517e69aadc8adb2604aa32926ff12a676
percyosag <percybuilder@gmail.com>
2024-12-24 03:05:23 -0500

    Button component made reusable

--- target/src/App.js
+++ target/src/App.js
@@ -41,23 +41,27 @@ function App() {
             Step {step}: {messages[step - 1]}{" "}
           </p>
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handlePrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handleNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
+              <span>👈</span>Previous
+            </Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
+              Next<span>👉</span>
+            </Button>
           </div>
         </div>
       )}
     </>
   );
 }
+const Button = ({ textColor, bgColor, onClick, children }) => {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default App;
